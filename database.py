67600fe3b77de42d99d5c3b76139a84d052d2b4b
areeb <areeb290509@gmail.com>
2026-08-13 19:58:41 +0530

    Postgres shift

--- database.py
+++ database.py
@@ -1,13 +1,23 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLite database file (created automatically in the project folder)
-SQLALCHEMY_DATABASE_URL = "sqlite:///./skill_exchange.db"
+# Read the database URL from the environment variable.
+# If DATABASE_URL is set, use PostgreSQL.
+# If it is not set, fall back to SQLite for local testing.
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+if DATABASE_URL:
+    # PostgreSQL (production)
+    engine = create_engine(DATABASE_URL)
+else:
+    # SQLite database file (created automatically in the project folder)
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./skill_exchange.db"
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
 
 # SessionLocal is used to create database sessions for each request
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
